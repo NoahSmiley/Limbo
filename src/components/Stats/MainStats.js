e3c2cf5e1e579bb11ac1164d38b91a1ac16fe191
Noah Smiley <noahsmiley123@gmail.com>
2022-02-14 15:29:42 -0600

    Implemented Statistics Page

---
+++ src/components/Stats/MainStats.js
@@ -0,0 +1,89 @@
+import {
+  Row,
+  Col,
+  Button,
+  Statistic,
+  Card,
+  List,
+  Divider,
+  Typography,
+  Descriptions
+} from "antd";
+import { ArrowUpOutlined, ArrowDownOutlined } from "@ant-design/icons";
+
+const MainStats = () => {
+  const data = [
+    "Racing car sprays burning fuel into crowd.",
+    "Japanese princess to wed commoner.",
+    "Australian walks 100km after outback crash.",
+    "Man charged over missing wedding girl.",
+    "Los Angeles battles huge wildfires.",
+  ];
+  return (
+    <Row gutter={16}>
+      <Col span={12}>
+        <Card>
+          <Statistic title="Active Nodes" value={2} />
+        </Card>
+      </Col>
+      <Col span={12}>
+        <Card>
+          <Statistic title="Current Credits (Lim)" value={3} />
+        </Card>
+      </Col>
+      <Col span={12}>
+        <Card>
+          <Statistic
+            title="Credit Increase"
+            value={11.28}
+            precision={2}
+            valueStyle={{ color: "#3f8600" }}
+            prefix={<ArrowUpOutlined />}
+            suffix="%"
+          />
+        </Card>
+      </Col>
+      <Col span={12}>
+        <Card>
+          <Statistic
+            title="Current Hash Rate"
+            value={4}
+            precision={2}
+            valueStyle={{ color: "#cf1322" }}
+            prefix={<ArrowDownOutlined />}
+            suffix="ms"
+          />
+        </Card>
+      </Col>
+      <Divider orientation="left">Block Ledger:</Divider>
+      <List
+        size="large"
+        style={{textAlign:"left"}}
+        header={<div>Transactions:</div>}
+        footer={<div>Footer</div>}
+        bordered
+        dataSource={data}
+        renderItem={(item) => (
+          <List.Item>
+            <Descriptions title="Node 2347">
+              <Descriptions.Item label="UserName">
+                Zhou Maomao
+              </Descriptions.Item>
+              <Descriptions.Item label="Action">
+                Post
+              </Descriptions.Item>
+              <Descriptions.Item label="Live">
+                Hangzhou, Zhejiang
+              </Descriptions.Item>
+              <Descriptions.Item label="Remark">empty</Descriptions.Item>
+              <Descriptions.Item label="Address">
+                No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
+              </Descriptions.Item>
+            </Descriptions>
+          </List.Item>
+        )}
+      />
+    </Row>
+  );
+};
+export default MainStats;
--- src/pages/connected/Connected.js
+++ src/pages/connected/Connected.js
@@ -1,7 +1,39 @@
-import LayOut from "../../components/Base/LayOut";
+import FullLayOut from "../../components/Base/FullLayOut";
 import ConnectedResult from "../../components/Results/ConnectedResult";
-
-const Connected = ()=>{
-    return(<LayOut><ConnectedResult /></LayOut>);
-}
+import { Modal, Button } from "antd";
+import { useState } from "react";
+import MainStats from "../../components/Stats/MainStats";
+const Connected = () => {
+  const [isModalVisible, setIsModalVisible] = useState(true);
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+  return (
+    <div>
+      <Modal
+        style={{ marginLeft: "38%" }}
+        title="Node Connection Staus"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={[
+          <Button key="submit" type="primary" onClick={handleOk}>
+            Sounds Good!
+          </Button>,
+        ]}
+      >
+        <ConnectedResult />
+      </Modal>
+      <FullLayOut titleone="Limbo"titletwo="Node">
+        <MainStats />
+      </FullLayOut>
+    </div>
+  );
+};
 export default Connected;
