7fa46ee36f8c0a776aa4a0bb4a597377db16bc80
Noah Smiley <noahsmiley123@gmail.com>
2022-02-14 15:28:35 -0600

    Loading bug fixed (loads gracefully now)

--- public/main.js
+++ public/main.js
@@ -1,9 +1,9 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require("electron");
 
-const path = require('path')
-const isDev = require('electron-is-dev')
+const path = require("path");
+const isDev = require("electron-is-dev");
 
-require('@electron/remote/main').initialize()
+require("@electron/remote/main").initialize();
 
 function createWindow() {
   // Create the browser window.
@@ -12,32 +12,37 @@ function createWindow() {
     height: 768,
     minHeight: 900,
     minWidth: 700,
+    show: false,
     webPreferences: {
       nodeIntegration: true,
-      enableRemoteModule: true
-    }
-  })
+      enableRemoteModule: true,
+    },
+  });
+
+  win.once("ready-to-show", () => {
+    win.show();
+  });
 
   win.loadURL(
     isDev
-      ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`
-  )
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on("window-all-closed", function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', function () {
+app.on("activate", function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
