6238ca119eaa4bc7bb4cba4c7323f90bcb41f2b5
Riley Edmunds <rileyedmunds7@gmail.com>
2022-03-17 19:42:49 -0500

    Adding more Docs

--- src/pages/connected/Docs.js
+++ src/pages/connected/Docs.js
@@ -6,12 +6,16 @@ const Docs = () => {
       <Card title="Limbo Documentation" style={{marginTop:"-15%",textAlign:"left"}}>
         <Card
           type="inner"
-          title="Mevo"
+          title="What is Mevo?"
         >
           Mevo is just like the most popular social media applications with a bit of a twist.
-          Our application will not store data in a central location but, will use Google Firebase 
-          to give all users with accounts there own personal blockchain with application data.
-
+          The main motivation behined Mevo is to provide a place were people can meet and communicate 
+          without being held to any societal standards by the moderaters of the application.
+          In doing this our application will not store data in a central location but, will use Google 
+          Firebase to give all users with accounts there own personal blockchain with application data.
+          On top of that all data will be encripted using the popular sha-256 to ensure users sensitive
+          data remains safe. Now you might be thinking what does blockchain have to do with this?
+          That will be discussed in the documentation below.
         </Card>
         <Card
           style={{ marginTop: 16 }}
@@ -27,7 +31,7 @@ const Docs = () => {
         >
           Blockchains have recently become a buzz word with the rise of cryptocurrencies. 
           However, blockchains are not limited to cryptocurrencies they are simply a way of 
-          storing data.
+          storing data. 
         </Card>
       </Card>
       ,
