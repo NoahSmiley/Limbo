7e36c37dcba5b8826160de633e53ce4615ded387
Riley Edmunds <rileyedmunds7@gmail.com>
2022-03-17 20:04:59 -0500

    Why Blockchain

--- src/pages/connected/Docs.js
+++ src/pages/connected/Docs.js
@@ -9,11 +9,11 @@ const Docs = () => {
           title="What is Mevo?"
         >
           Mevo is just like the most popular social media applications with a bit of a twist.
-          The main motivation behined Mevo is to provide a place were people can meet and communicate 
-          without being held to any societal standards by the moderaters of the application.
+          The main motivation behind Mevo is to provide a place where people can meet and communicate 
+          without being held to any societal standards by the moderators of the application.
           In doing this our application will not store data in a central location but, will use Google 
-          Firebase to give all users with accounts there own personal blockchain with application data.
-          On top of that all data will be encripted using the popular sha-256 to ensure users sensitive
+          Firebase to give all users with accounts their own personal blockchain with application data.
+          On top of that all data will be encrypted using the popular sha-256 to ensure users sensitive
           data remains safe. Now you might be thinking what does blockchain have to do with this?
           That will be discussed in the documentation below.
         </Card>
@@ -22,7 +22,7 @@ const Docs = () => {
           type="inner"
           title="How our Application Works"
         >
-          <pre>Hello</pre>
+          This application is 
         </Card>
         <Card
           style={{ marginTop: 16 }}
@@ -31,7 +31,19 @@ const Docs = () => {
         >
           Blockchains have recently become a buzz word with the rise of cryptocurrencies. 
           However, blockchains are not limited to cryptocurrencies they are simply a way of 
-          storing data. 
+          storing data. We use blockchains as a way maintaining mutual power between all users.
+          We found this a necessity to eleminate the posibility of rouge administrators, third
+          party tampering, and other power heiarchys. Blockchain does this by being imumtable,
+          meaning that data once on the blockchain cannot be alterd or deleted. Therefore, if 
+          a user is to do any action that needs to be added to the blockchain such as making
+          a post it cannot be erased.
+        </Card>
+        <Card
+          style={{ marginTop: 16 }}
+          type="inner"
+          title="Future Functions"
+        >
+          This application is 
         </Card>
       </Card>
       ,
